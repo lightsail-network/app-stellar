98f783c57c7518d8441c3eb7903db4079a8c1401
Jun Luo <4catcode@gmail.com>
2024-04-21 16:16:22 +0800

    fuzz

--- libstellar/include/stellar/formatter.h
+++ libstellar/include/stellar/formatter.h
@@ -3,8 +3,7 @@
 #include "types.h"
 #include "buffer.h"
 
-typedef struct
-{
+typedef struct {
     const uint8_t *raw_data;
     size_t raw_data_len;
     envelope_t *envelope;
@@ -16,6 +15,6 @@ typedef struct
     bool display_sequence;
 } formatter_data_t;
 
-
 void reset_formatter();
+
 bool get_next_data(formatter_data_t *fdata, bool forward, bool *data_exists, bool *is_op_header);
--- libstellar/include/stellar/parser.h
+++ libstellar/include/stellar/parser.h
@@ -8,4 +8,7 @@
 
 bool parse_transaction_envelope(const uint8_t *data, size_t data_len, envelope_t *envelope);
 
-bool parse_transaction_operation(const uint8_t *data, size_t data_len, envelope_t *envelope, uint8_t operation_index);
+bool parse_transaction_operation(const uint8_t *data,
+                                 size_t data_len,
+                                 envelope_t *envelope,
+                                 uint8_t operation_index);
