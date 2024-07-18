003f23424b3a675061e5947dc941f0eb5f047be4
Jun Luo <4catcode@gmail.com>
2024-07-19 07:39:12 +0800

    wip

--- scripts/generate_token_list.py
+++ scripts/generate_token_list.py
@@ -4,6 +4,39 @@ from stellar_sdk import Asset, Network, Address, utils
 from stellar_sdk import xdr as stellar_xdr
 
 LOBSTR_TOKEN_API_URL = "https://lobstr.co/api/v1/sep/assets/curated.json"
+SOROSWAP_TOKEN_API_URL = (
+    "https://raw.githubusercontent.com/soroswap/token-list/main/tokenList.json"
+)
+STELLAR_EXPERT_TOKEN_API_URL = (
+    "https://api.stellar.expert/explorer/public/asset/?limit=50&order=desc&sort=rating"
+)
+
+lobstr_tokens = set()
+soroswap_tokens = set()
+stellar_expert_tokens = set()
+
+resp = requests.get(LOBSTR_TOKEN_API_URL).json()
+for record in resp["assets"]:
+    asset = Asset(record["code"], record["issuer"])
+    lobstr_tokens.add(asset)
+
+resp = requests.get(SOROSWAP_TOKEN_API_URL).json()
+for record in resp["assets"]:
+    asset = Asset(record["code"], record["issuer"])
+    soroswap_tokens.add(asset)
+
+resp = requests.get(STELLAR_EXPERT_TOKEN_API_URL).json()
+for record in resp["_embedded"]["records"]:
+    asset = record["asset"]
+    if len(asset.split("-")) == 1:
+        continue
+    else:
+        asset_code, asset_issuer = asset.split("-")[:2]
+        asset = Asset(asset_code, asset_issuer)
+        stellar_expert_tokens.add(asset)
+
+# Find the common tokens
+common_tokens = lobstr_tokens & soroswap_tokens & stellar_expert_tokens
 
 
 def get_asset_contract_id(asset: Asset, network_passphrase: str) -> bytes:
@@ -29,7 +62,5 @@ def print_asset(asset):
     print(f'{{{{{"".join([f"0x{x:02x}," for x in contract_id])}}}, "{asset.code}"}},')
 
 
-resp = requests.get(LOBSTR_TOKEN_API_URL).json()
-for record in resp["assets"]:
-    asset = Asset(record["code"], record["issuer"])
+for asset in common_tokens:
     print_asset(asset)
